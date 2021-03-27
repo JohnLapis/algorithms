7622c3be649c465d6fc63a77b28be1de12f1d02e
john <borges-2014@live.com>
2021-03-27 10:19:45 -0300

    refactor: cpf_generator in hasekll

--- misc/cpf_generator.hs
+++ misc/cpf_generator.hs
@@ -1,29 +1,24 @@
 import System.Random
 import Control.Monad
 import Control.Monad.IO.Class
+import Data.List
 
 randDigits :: MonadIO m => Int -> m [Int]
 randDigits n = replicateM n (randomRIO (0 :: Int, 9))
 
-addVd1 :: Integral a => [a] -> [a]
-addVd1 cpf =
-  let value = mod (sum (zipWith (*) [10,9..1] cpf)) 11
-  in cpf ++ [if value < 2 then 0 else 11 - value]
+genVd1 :: Integral a => [a] -> a
+genVd1 cpf = if value < 2 then 0 else 11 - value
+  where value = mod (sum (zipWith (*) [10,9..1] cpf)) 11
 
-addVd2 :: Integral a => [a] -> [a]
-addVd2 cpf =
-  let value = mod (sum (zipWith (*) [11,9..1] cpf)) 11
-  in cpf ++ [if value < 2 then 0 else 11 - value]
+genVd2 :: Integral a => [a] -> a
+genVd2 cpf = if value < 2 then 0 else 11 - value
+  where value = mod (sum (zipWith (*) [11,9..1] cpf)) 11
 
 cpfGenerator :: MonadIO m => m [Int]
 cpfGenerator = do
-  nineDigits <- randDigits 9
-  return $ addVd2 $ addVd1 nineDigits
-
-joinListBy :: Show a => String -> [a] -> String
-joinListBy _ [] = ""
-joinListBy delim (x:xs) = show x ++ delim ++ joinListBy delim xs
+  digits <- randDigits 9
+  return $ digits ++ [genVd1 digits, genVd2 digits]
 
 main = do
   cpf <- cpfGenerator
-  putStrLn $ joinListBy "" cpf
+  putStrLn . intercalate "" . map show $ cpf
