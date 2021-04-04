be282e7d9a9d6e9de775c26065ae503e3bf78fb4
john <borges-2014@live.com>
2021-04-03 21:17:09 -0300

    fix: Cpf generator in Haskell

--- misc/cpf_generator.hs
+++ misc/cpf_generator.hs
@@ -8,16 +8,18 @@ randDigits n = replicateM n (randomRIO (0 :: Int, 9))
 
 genVd1 :: Integral a => [a] -> a
 genVd1 cpf = if value < 2 then 0 else 11 - value
-  where value = mod (sum (zipWith (*) [10,9..1] cpf)) 11
+  where value = sum (zipWith (*) [10,9..1] cpf) `mod` 11
 
 genVd2 :: Integral a => [a] -> a
 genVd2 cpf = if value < 2 then 0 else 11 - value
-  where value = mod (sum (zipWith (*) [11,9..1] cpf)) 11
+  where value = sum (zipWith (*) [11,10..1] cpf) `mod` 11
 
 cpfGenerator :: MonadIO m => m [Int]
 cpfGenerator = do
   digits <- randDigits 9
-  return $ digits ++ [genVd1 digits, genVd2 digits]
+  let vd1 = genVd1 digits
+      vd2 = genVd2 $ digits ++ [vd1]
+  return $ digits ++ [vd1, vd2]
 
 main = do
   cpf <- cpfGenerator
